45d9a3626fd56eb265d498d3412b0917f1ea4a23
Otsuki Hitoshi <hitochan777@gmail.com>
2024-05-23 01:21:15 +0900

    Fix compilation error

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,23 @@ async fn main() -> Result<CombinedService, shuttle_runtime::Error> {
     );
     let cloned_peer_status_repository = peer_status_repository.clone();
     let job: Box<dyn Send + 'static + Fn() -> Box<dyn Send + 'static + Future<Output = ()>>> =
-        Box::new(|| {
+        Box::new(move || {
+            println!("checking disconnected peers...");
+            let peer_status_repository = peer_status_repository.clone();
             Box::new(async move {
-                peer_status_repository
-                    .fetch_one(String::from("peer1"), String::from("peer2"))
-                    .await;
-                // for user_id in cloned_peer_status_repository.fetch_user_ids().await.unwrap() {
-                //     peer_status_repository.fetch_all_by_user_id(user_id).await;
-                // }
+                for user_id in peer_status_repository.fetch_user_ids().await.unwrap() {
+                    let statuses = peer_status_repository
+                        .fetch_all_by_user_id(user_id)
+                        .await
+                        .unwrap();
+
+                    for status in statuses {
+                        // if status.is_disconnected() {
+                        //     println!("{} is disconnected", status.peer_id);
+                        // }
+                        // leader.disconnect(peer_id)
+                    }
+                }
             })
         });
     Ok(CombinedService::new(
